4a7302710ea1862a341acd58783e377a282c0b00
mdsiam1820 <hafiz.siam15@gmail.com>
2025-06-25 14:09:37 +0600

    Update adduser.js

--- Script/commands/adduser.js
+++ Script/commands/adduser.js
@@ -2,7 +2,7 @@ module.exports.config = {
 	name: "adduser",
 	version: "2.4.3",
 	hasPermssion: 0,
-	credits: "𝐂𝐘𝐁𝐄𝐑 ☢️_𖣘 -𝐁𝐎𝐓 ⚠️ 𝑻𝑬𝑨𝑴_ ☢️",
+	credits: "👑 𝐀𝐝𝐦𝐢𝐧 • 𝐁𝐨𝐬𝐬 𝐒𝐢𝐚𝐦 ☢️",
 	description: "Add user to the group by link or id",
 	commandCategory: "group",
 	usages: "[args]",
